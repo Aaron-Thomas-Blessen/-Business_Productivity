21b7b3113ac4c80044512815cb1a6d109d5feb6c
Aaron Thomas Blessen <aaronthomas232200@gmail.com>
2025-03-30 23:24:15 +0530

    test.js update to inclue intelligent and action

--- Agents/test.js
+++ Agents/test.js
@@ -1,16 +1,25 @@
-
 import { companyKnowledge } from './companyKnowledge.js';
-import {generateText} from './generateText.js';
+import { generateText as generateTextBase } from './generateText.js';
 import { workPlaceOptimization } from './workplaceOptimization.js';
+import { generateText as actionText } from './Action.js';
+import { generateText as intelligentText } from './Intelligent.js';
 
 (async () => {
   try {
     const workplaceOptimizationResult = await workPlaceOptimization();
-    console.log(workplaceOptimizationResult);
+    console.log('Workplace Optimization Result:', workplaceOptimizationResult);
+    
     const companyKnowledgeResult = await companyKnowledge();
-    console.log(companyKnowledgeResult);
-    const textResult = await generateText();
-    console.log(textResult);
+    console.log('Company Knowledge Result:', companyKnowledgeResult);
+    
+    const textResult = await generateTextBase();
+    console.log('Base Text Result:', textResult);
+    
+    const actionResult = await actionText();
+    console.log('Action Items Result:', actionResult);
+    
+    const intelligentResult = await intelligentText();
+    console.log('Intelligent Email Result:', intelligentResult);
     
   } catch (error) {
     console.error('Error:', error.message);
